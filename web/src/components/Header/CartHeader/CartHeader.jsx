63fa6f5d8ced4e2cd558fc804dbd5e14b7b901d5
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2024-08-12 23:43:24 -0300

    chore: Refactor CartHeader and Header components, and update Cart and Header styles

--- web/src/components/Header/CartHeader/CartHeader.jsx
+++ web/src/components/Header/CartHeader/CartHeader.jsx
@@ -1,14 +1,17 @@
 import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../../context/CartContext';
 
 export default function CartHeader() {
   const { cart } = useContext(CartContext);
+  const navigate = useNavigate();
+
   return (
-    <div>
+    <button type="button" onClick={() => navigate('/cart')}>
       <p className="text-white font-bold text-sm">Meu carrinho</p>
       <p className="text-end text-xs text-[#a3a3a6]">
         <span>{cart.length}</span> itens
       </p>
-    </div>
+    </button>
   );
 }
--- web/src/components/Header/Header.jsx
+++ web/src/components/Header/Header.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import Logo from '../../assets/Logo';
 import CartHeader from './CartHeader/CartHeader';
 
 export default function Header() {
+  const navigate = useNavigate();
 
   return (
     <div className="flex flex-col h-full px-60 items-center bg-[#191920]">
       <div className="flex w-full justify-between py-12">
-        <div className="flex gap-3 text-white">
+        <button type="button" className="flex gap-3 text-white" onClick={() => navigate('/')}>
           <p className=" text-4xl font-extrabold">COGNYSHOES</p>
           <Logo />
-        </div>
+        </button>
         <CartHeader />
       </div>
       <Outlet />
