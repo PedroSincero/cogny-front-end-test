787bd13302257294752820cc47e4903d00026e05
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2024-08-13 03:27:08 -0300

    feat: Add CartAction component to ProductItem

---
+++ mobile/src/components/ProductItem/CartAction/CartAction.jsx
@@ -0,0 +1,37 @@
+import React, { useContext, useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { updateProductInOrder } from '../../../services/orders';
+import { CartContext } from '../../../context/CartContext';
+
+export default function CartAction({ description, imageUrl, price, id }) {
+  const [quantity, setQuantity] = useState(1);
+  const { setCart } = useContext(CartContext);
+
+  const handleQuantity = (text) => {
+    const result = Number(text);
+    setQuantity(result);
+  };
+
+  const handleAddToCart = async () => {
+    const cart = await updateProductInOrder(id, { quantity, description, imageUrl, price, id });
+    setCart(cart);
+  };
+
+  return (
+    <View className="flex-row font-bold text-white text-sm">
+      <TextInput
+        value={quantity.toString()}
+        keyboardType="numeric"
+        className="flex text-center text-white rounded-l-md py-1.5 px-7 bg-[#c62c4a]"
+        editable={false}
+        onChangeText={handleQuantity}
+      />
+      <TouchableOpacity
+        className="flex-1 rounded-r-md bg-[#F8375D] py-2.5 hover:bg-[#c62c4a]"
+        onPress={handleAddToCart}
+      >
+        <Text  className="flex text-center font-semibold text-white">ADICIONAR</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
