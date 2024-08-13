131f254e9c4fcebfadf9c97a862b435e52b8413c
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2024-08-13 18:30:08 -0300

    chore: Update Cart component to display product details in a table

--- web/src/pages/Cart/Table/Body/ProductCell/ProductCell.jsx
+++ web/src/pages/Cart/Table/Body/ProductCell/ProductCell.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 
 export default function ProductCell({ description, imageUrl, price }) {
   return (
-    <td className="py-4">
-      <div className="flex items-center">
-        <img src={imageUrl} alt={description} className="w-20 h-20 object-cover mr-4" />
-        <div>
-          <p className="font-bold text-sm text-[#333333]">{description}</p>
-          <p className="font-bold text-black">R${price.toFixed(2)}</p>
+    <>
+      <td className="py-4 pr-5 w-32 h-32">
+        <img src={imageUrl} alt={description} className="w-full h-full object-cover mr-4" />
+      </td>
+      <td className="py-4">
+        <div className="flex items-center">
+          <div>
+            <p className="font-bold text-sm text-[#333333]">{description}</p>
+            <p className="font-bold text-black">R${price.toFixed(2)}</p>
+          </div>
         </div>
-      </div>
-    </td>
+      </td>
+    </>
   );
 }
--- web/src/pages/Cart/Table/Head/Head.jsx
+++ web/src/pages/Cart/Table/Head/Head.jsx
@@ -4,9 +4,10 @@ export default function Head() {
   return (
     <thead className="text-[#999999]">
       <tr>
+        <th className="p-2" aria-label="Empty Header" />
         <th className="text-left p-2">PRODUTO</th>
         <th className="text-center p-2">QTD</th>
-        <th className="text-right p-2">PREÇO</th>
+        <th className="text-right">PREÇO</th>
       </tr>
     </thead>
   );
